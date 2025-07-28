87947bc361220e116a1af40d4951934f48ee8acd
Xuan Hung <hungnguyen.250404@gmail.com>
2025-07-28 23:30:46 +0700

    UpdateChart

---
+++ SmartWearAdmin/Controllers/StatisticsController.cs
@@ -0,0 +1,41 @@
+﻿using Business.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartWearAdmin.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StatisticsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            // Trả về danh mục dưới dạng JSON
+            var categories = _context.Categories
+                .Select(c => new { c.Id, c.Name })
+                .ToList();
+
+            return Json(categories);
+        }
+
+        [HttpGet]
+        public IActionResult GetProductsByCategory(Guid categoryId)
+        {
+            var data = _context.Products
+                .Where(p => p.CategoryId == categoryId)
+                .Select(p => new
+                {
+                    ProductName = p.Name,
+                    StockQuantity = p.StockQuantity
+                }).ToList();
+
+            return Json(data);
+        }
+    }
+}
